7f512263090b67289518af0c70d19090d79de8d6
rstuannm <tuannm@runsystem.net>
2018-11-06 08:46:25 +0700

    add
    add

---
+++ MVCProject/MVCProject/Controllers/KhachhangController.cs
@@ -0,0 +1,31 @@
+﻿using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class KhachhangController : Controller
+    {
+        // GET: Khachhang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(KhachHangVM model)
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return Json(new { status = true,msgg = "thanh cong" });
+        }
+    }
+}
